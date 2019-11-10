1174963f43c38313ea6b214d8cb9e59505d33d4e
devizer <devizer@users.github.com>
2019-11-10 20:18:40 +0200

    Test1

--- Universe.CpuUsage.Tests/UnitTest1.cs
+++ Universe.CpuUsage.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Universe.CpuUsage.Tests
@@ -7,11 +8,13 @@ namespace Universe.CpuUsage.Tests
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine("I'm SETUP'");
         }
 
         [Test]
         public void Test1()
         {
+            Console.WriteLine("I'm TEST");
             Assert.Pass();
         }
     }
