065a405fea62e3e818ca35f1ba20779310f6e24d
devizer <devizer@users.github.com>
2019-11-11 21:15:24 +0200

    port_deallocate commented out

--- Tests4Mac/on-local-mac.sh
+++ Tests4Mac/on-local-mac.sh
@@ -1,6 +1,5 @@
 work=$HOME/build/devizer; rm -rf $work; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; 
-cd Universe.CpuUsage
-dotnet build -c Release
-dotnet test -f netcoreapp3.0
+cd Universe.CpuUsage/Tests4Mac
+bash -e test-using-mono-only.sh 
 
 
--- Universe.CpuUsage/MacOsThreadInfo.cs
+++ Universe.CpuUsage/MacOsThreadInfo.cs
@@ -15,36 +15,39 @@ namespace Universe.CpuUsage
 
         static CpuUsage? Get()
         {
-            int threadId = MacOsThreadInfoInterop.mach_thread_self();
-            try
+            if (false)
             {
-                if (threadId == 0) return null;
-
-                var ret = MacOsThreadInfoInterop.GetThreadInfo(threadId);
-                return ret;
-            }
-            finally
-            {
-                int? self = null;
-                int kResult2 = -424242;
-                int kResult1 =
-                    MacOsThreadInfoInterop.mach_port_deallocate(threadId, threadId);
-
-                // https://opensource.apple.com/source/xnu/xnu-792/osfmk/mach/kern_return.h
-                // KERN_INVALID_TASK 16: target task isn't an active task.
-                if (kResult1 != 0)
+                int threadId = MacOsThreadInfoInterop.mach_thread_self();
+                try
                 {
-                    self = MacOsThreadInfoInterop.mach_thread_self();
-                    kResult2 = MacOsThreadInfoInterop.mach_port_deallocate(self.Value, threadId);
+                    if (threadId == 0) return null;
+
+                    var ret = MacOsThreadInfoInterop.GetThreadInfo(threadId);
+                    return ret;
                 }
-#if DEBUG || true
-                if (kResult1 != 0 || kResult2 != 0)
+                finally
                 {
-                    Console.WriteLine($@"{(kResult1 == 0 || kResult2 == 0 ? "Info" : "Warning!!!!!")} 
+                    int? self = null;
+                    int kResult2 = -424242;
+                    int kResult1 =
+                        MacOsThreadInfoInterop.mach_port_deallocate(threadId, threadId);
+
+                    // https://opensource.apple.com/source/xnu/xnu-792/osfmk/mach/kern_return.h
+                    // KERN_INVALID_TASK 16: target task isn't an active task.
+                    if (kResult1 != 0)
+                    {
+                        self = MacOsThreadInfoInterop.mach_thread_self();
+                        kResult2 = MacOsThreadInfoInterop.mach_port_deallocate(self.Value, threadId);
+                    }
+#if DEBUG || true
+                    if (kResult1 != 0 || kResult2 != 0)
+                    {
+                        Console.WriteLine($@"{(kResult1 == 0 || kResult2 == 0 ? "Info" : "Warning!!!!!")} 
     mach_port_deallocate({threadId}, {threadId}) returned {kResult1}
     mach_port_deallocate(mach_thread_self() == {self}, {threadId}) returned {kResult2}");
-                }
+                    }
 #endif
+                }
             }
 
         }
