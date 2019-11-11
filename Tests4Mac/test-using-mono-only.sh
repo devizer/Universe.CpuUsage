76a56dace5664c11f1ff1460d78d8b121ea48e68
devizer <devizer@users.github.com>
2019-11-11 08:02:50 +0200

    copy paste for mac os

--- Tests4Mac/test-using-mono-only.sh
+++ Tests4Mac/test-using-mono-only.sh
@@ -1,5 +1,15 @@
 # work=$HOME/build/devizer; rm -rf $work; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage/Tests4Mac
 
+    function header() {
+      if [[ $(uname -s) != Darwin ]]; then
+        startAt=${startAt:-$(date +%s)}; elapsed=$(date +%s); elapsed=$((elapsed-startAt)); elapsed=$(TZ=UTC date -d "@${elapsed}" "+%_H:%M:%S");
+      fi
+      LightGreen='\033[1;32m'; Yellow='\033[1;33m'; RED='\033[0;31m'; NC='\033[0m'; LightGray='\033[1;2m';
+      printf "${LightGray}${elapsed:-}${NC} ${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; 
+    }
+    counter=0; function Say() { echo ""; counter=$((counter+1)); header "STEP $counter" "$1"; }; Say "" >/dev/null
+
+
 echo '<?xml version="1.0" encoding="utf-8"?>
 <configuration>
   <packageSources>
@@ -22,3 +32,34 @@ cd ../Tests4Linux
 nuget restore
 pushd ~/build/devizer/Universe.CpuUsage; find . ; popd
 msbuild /t:Rebuild /p:Configuration=Release
+
+      proj=Universe.CpuUsage.MonoTests/Universe.CpuUsage.MonoTests.csproj
+      cp -f ${proj} ${proj}-bak
+      for target_dir in $(ls -d bin/*/); do
+        target=$(basename $target_dir)
+
+        Say "Mono Tests: msbuild rebuild for [$target]"
+        sed_cmd="s/\.\.\\bin\\net20\\Universe/\.\.\\bin\\${target}\\Universe/g"
+        sed_cmd="s/net20/${target}/g"
+        
+        cp -f ${proj}-bak ${proj}
+        sed -i "$sed_cmd" $proj
+        echo "REF: $(cat $proj | grep $target)"
+        
+        cfg=Release
+        msbuild /t:Rebuild /p:Configuration=$cfg /v:q
+        
+        pushd packages/NUnit.ConsoleRunner*/tools
+        runner=$(pwd)/nunit3-console.exe
+        popd
+
+        Say "Mono Tests: Run Tests for [$target]"
+        pushd Universe.CpuUsage.MonoTests/bin/$cfg
+        $runner --workers=1 Universe.CpuUsage.MonoTests.exe --result=MonoTests.xml;format=AppVeyor || (Say "TESTING NET 2.0 ERROR"; exit 666)
+        popd
+      
+      done
+
+      
+      popd
+      Say "Mono Tests: Done"
