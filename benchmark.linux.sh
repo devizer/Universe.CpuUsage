ded389bff8857901a8272633398b9a905cc8c902
devizer <devizer@users.github.com>
2019-11-12 07:01:20 +0200

    fix: target framework for macosx

--- benchmark.linux.sh
+++ benchmark.linux.sh
@@ -2,7 +2,7 @@
 # work=$HOME/build/devizer; rm -rf $work; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage
 # dotnet tool install -g BenchmarkDotNet.Tool
 pushd Universe.CpuUsage.Banchmark
-dotnet publish -o bin/benchmark -c Release
+dotnet publish -o bin/benchmark -c Release -f netcoreapp3.0
 pushd bin/benchmark 
 dotnet benchmark Universe.CpuUsage.Banchmark.dll --filter *CpuUsageBenchmarks*
 popd
