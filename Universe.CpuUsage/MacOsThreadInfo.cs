41156ca2dfd252cae1d85929d6540a774ec7968b
devizer <devizer@users.github.com>
2019-11-12 04:14:37 +0200

    revert mach_port_deallocate

--- Universe.CpuUsage/MacOsThreadInfo.cs
+++ Universe.CpuUsage/MacOsThreadInfo.cs
@@ -25,29 +25,26 @@ namespace Universe.CpuUsage
             }
             finally
             {
-                if (false)
-                {
-                    int? self = null;
-                    int kResult2 = -424242;
-                    int kResult1 =
-                        MacOsThreadInfoInterop.mach_port_deallocate(threadId, threadId);
+                int? self = null;
+                int kResult2 = -424242;
+                int kResult1 =
+                    MacOsThreadInfoInterop.mach_port_deallocate(threadId, threadId);
 
-                    // https://opensource.apple.com/source/xnu/xnu-792/osfmk/mach/kern_return.h
-                    // KERN_INVALID_TASK 16: target task isn't an active task.
-                    if (kResult1 != 0)
-                    {
-                        self = MacOsThreadInfoInterop.mach_thread_self();
-                        kResult2 = MacOsThreadInfoInterop.mach_port_deallocate(self.Value, threadId);
-                    }
-#if DEBUG || true
-                    if (kResult1 != 0 || kResult2 != 0)
-                    {
-                        Console.WriteLine($@"{(kResult1 == 0 || kResult2 == 0 ? "Info" : "Warning!!!!!")} 
+                // https://opensource.apple.com/source/xnu/xnu-792/osfmk/mach/kern_return.h
+                // KERN_INVALID_TASK 16: target task isn't an active task.
+                if (kResult1 != 0)
+                {
+                    self = MacOsThreadInfoInterop.mach_thread_self();
+                    kResult2 = MacOsThreadInfoInterop.mach_port_deallocate(self.Value, threadId);
+                }
+#if DEBUG 
+                if (kResult1 != 0 && kResult2 != 0)
+                {
+                    Console.WriteLine($@"{(kResult1 == 0 || kResult2 == 0 ? "Info" : "Warning!!!!!")} 
     mach_port_deallocate({threadId}, {threadId}) returned {kResult1}
     mach_port_deallocate(mach_thread_self() == {self}, {threadId}) returned {kResult2}");
-                    }
-#endif
                 }
+#endif
             }
 
         }
