3bf57713f381b1088b7c8d8c24448ca60905b7af
devizer <devizer@users.github.com>
2019-11-12 02:44:00 +0200

    simple benchmark 2

--- benchmark.linux.sh
+++ benchmark.linux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# work=$HOME/build/devizer; rm -rf $work; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage
+# dotnet tool install -g BenchmarkDotNet.Tool
 pushd Universe.CpuUsage.Banchmark
 dotnet publish -o bin/benchmark -c Release
-pushd bin\benchmark 
+pushd bin/benchmark 
 dotnet benchmark Universe.CpuUsage.Banchmark.dll --filter *CpuUsageBenchmarks*
 popd
 popd
