88399dba44aa28505e8defe7e89518d8ff336f2b
devizer <devizer@users.github.com>
2019-11-21 13:25:39 +0200

    cleanup

--- Universe.CpuUsage.Tests/RisingTest.cs
+++ Universe.CpuUsage.Tests/RisingTest.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 using NUnit.Framework;
 using Tests;
 
