b676d3d18588e6b0aacb506816d6596fe03864f7
devizer <devizer@users.github.com>
2019-11-23 02:34:48 +0200

    OSX tests

--- Tests4Mono/build-the-matrix.sh
+++ Tests4Mono/build-the-matrix.sh
@@ -1,6 +1,6 @@
 # work=$HOME/build/devizer; mkdir -p $work; cd $work; rm -rf Universe.CpuUsage; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage/Tests4Mono; source build-the-matrix.sh; echo $matrix_run
 
-set -e
+# set -e
 
 # pushd "$(dirname "$0")" >/dev/null; SCRIPT="$(pwd)"; popd >/dev/null
 SCRIPT="$(pwd)"
@@ -117,4 +117,4 @@ if [[ $has_dot_net == yes && "$(uname -m)" == "aarch64" ]]; then
   popd
 fi
 
-set +e
+# set +e
