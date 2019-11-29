c1ba264600d3ebe30e38911b126320c7a16f6a46
devizer <devizer@users.github.com>
2019-11-30 00:35:43 +0200

    adding little makeup

--- Universe.CpuUsage.Tests/CpuUsageAsyncWatcher_Tests.cs
+++ Universe.CpuUsage.Tests/CpuUsageAsyncWatcher_Tests.cs
@@ -15,14 +15,14 @@ namespace Universe.CpuUsage.Tests
 
             await PreJit();
             CpuUsageAsyncWatcher watch = new CpuUsageAsyncWatcher();
-            await Task.Run(() => LoadCpu(111));
-            await Task.Run(() => LoadCpu(222));
-            await Task.Run(() => LoadCpu(333));
-            var actual = watch.GetTotalCpuUsage().TotalMicroSeconds;
-            var expected = 1000 * (111 + 222 + 333);
+            await Task.Run(() => LoadCpu(milliseconds: 111));
+            await Task.Run(() => LoadCpu(milliseconds: 222));
+            await Task.Run(() => LoadCpu(milliseconds: 333));
+            long actualMicroseconds = watch.GetTotalCpuUsage().TotalMicroSeconds;
+            long expected = 1000L * (111 + 222 + 333);
             Console.WriteLine(watch.ToHumanString(taskDescription:"SimpleTests()"));
-            // the 0.9 multiplier is need to compensate granularity and precision
-            Assert.Greater( actual / 0.9d, expected, "Cpu Usage in multi threaded scenario should be fully caught");
+            // the 0.99 multiplier is need to compensate granularity and precision
+            Assert.Greater( actualMicroseconds, expected * 0.99d , "Cpu Usage in multi threaded scenario should be fully caught");
         }
 
         [Test]
@@ -32,21 +32,21 @@ namespace Universe.CpuUsage.Tests
             
             await PreJit();
             CpuUsageAsyncWatcher watcher = new CpuUsageAsyncWatcher();
-            var task4 = Task.Run(() => LoadCpu(555));
-            var task3 = Task.Run(() => LoadCpu(444));
-            var task2 = Task.Run(() => LoadCpu(333));
-            var task1 = Task.Run(() => LoadCpu(222));
+            var task4 = Task.Run(() => LoadCpu(milliseconds: 555));
+            var task3 = Task.Run(() => LoadCpu(milliseconds: 444));
+            var task2 = Task.Run(() => LoadCpu(milliseconds: 333));
+            var task1 = Task.Run(() => LoadCpu(milliseconds: 222));
             Task.WaitAll(task1, task2, task3, task4);
             await NotifyFinishedTasks();
             var totalCpuUsage = watcher.Totals;
-            var actual = totalCpuUsage.GetSummaryCpuUsage().TotalMicroSeconds;
-            var expected = 1000 * (555 + 444 + 333 + 222);
+            long actualMicroseconds = totalCpuUsage.GetSummaryCpuUsage().TotalMicroSeconds;
+            long expected = 1000L * (555 + 444 + 333 + 222);
             Console.WriteLine(totalCpuUsage.ToHumanString(taskDescription:"ParallelTests()"));
-            // the 0.9 multiplier is need to compensate granularity and precision
-            Assert.Greater( actual / 0.9d, expected, "Cpu Usage in multi threaded scenario should be caught");
+            // the 0.99 multiplier is need to compensate granularity and precision
+            Assert.Greater( actualMicroseconds, expected * 0.99d, "Cpu Usage in multi threaded scenario should be caught");
         }
 
-        private void LoadCpu(int milliseconds) => CpuLoader.Run(milliseconds, true);
+        private void LoadCpu(int milliseconds = 42) => CpuLoader.Run(milliseconds, true);
 
         private async Task PreJit()
         {
@@ -57,15 +57,14 @@ namespace Universe.CpuUsage.Tests
 
         async Task NotifyFinishedTasks()
         {
-            // await Task.Run(() => Thread.Sleep(0));
-            await Task.Run(() => "Nothing to do" );
+            await Task.Run(() => "Nothing to do");
         }
 
         bool IsSupported()
         {
             if (!CpuUsageAsyncWatcher.IsSupported)
             {
-                Console.WriteLine("Skipping. CpuUsageAsyncWatcher requires NET 4.6+, Net Core 1.0+ or NetStandard 2.0+");
+                Console.WriteLine("Skipping. The CpuUsageAsyncWatcher class requires NET Framework 4.6+, Net Core 1.0+ or NetStandard 2.0+");
             }
 
             return CpuUsageAsyncWatcher.IsSupported;
