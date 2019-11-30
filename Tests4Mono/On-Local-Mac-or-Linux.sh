54ffa4d1116d36d4908dc898a723e077a47e88a8
devizer <devizer@users.github.com>
2019-12-01 00:23:51 +0200

    small cleanup again

--- Tests4Mono/On-Local-Mac-or-Linux.sh
+++ Tests4Mono/On-Local-Mac-or-Linux.sh
@@ -1,5 +1,2 @@
 work=$HOME/build/devizer; mkdir -p $work; cd $work; rm -rf $work/Universe.CpuUsage; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage/Tests4Mono; 
 git pull; set +e; source build-the-matrix.sh; echo $matrix_run; bash -e -c "$matrix_run"
-  
-
-
