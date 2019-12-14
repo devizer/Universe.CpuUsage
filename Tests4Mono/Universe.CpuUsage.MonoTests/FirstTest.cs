f24823b8a0a6d7bad749a0f27bafc13cf20def37
devizer <devizer@users.github.com>
2019-12-14 14:39:50 +0200

    added Target Framework Info for tests

--- Tests4Mono/Universe.CpuUsage.MonoTests/FirstTest.cs
+++ Tests4Mono/Universe.CpuUsage.MonoTests/FirstTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Mono.Cecil;
 using NUnit.Framework;
 
 namespace Universe.CpuUsage.MonoTests
@@ -12,7 +13,59 @@ namespace Universe.CpuUsage.MonoTests
             CpuUsage? usage = CpuUsage.GetByProcess();
             TestContext.Progress.WriteLine($"Process's CPU Usage: {usage}");
 
-            TestContext.Progress.WriteLine("CpuUsageReader Version: " + typeof(CpuUsage).Assembly.GetName().Version);
+            var cpuUsageVersion = typeof(CpuUsage).Assembly.GetName().Version;
+            TestContext.Progress.WriteLine($"CpuUsage Version: {cpuUsageVersion}");
+
+            var fileName = typeof(CpuUsage).Assembly.Location;
+
+            TestContext.Progress.WriteLine($"CpuUsage Location: {fileName}");
+
+
+
+            TryAndForget("ModuleDefinition.ReadModule()", () =>
+            {
+                ModuleDefinition module = ModuleDefinition.ReadModule(fileName);
+                ShowProperties(module, 4);
+                TestContext.Progress.WriteLine(" ");
+
+
+                foreach (CustomAttribute moduleCustomAttribute in module.CustomAttributes)
+                {
+                    TestContext.Progress.WriteLine($"Attribute: {moduleCustomAttribute.GetType()}");
+                    ShowProperties(moduleCustomAttribute, 4);
+                    TestContext.Progress.WriteLine(" ");
+                }
+
+
+            });
+            
+        }
+
+        void ShowProperties(object obj, int indent)
+        {
+            if (obj == null) return;
+            string pre = indent > 0 ? new string(' ', indent) : "";
+            var props = obj.GetType().GetProperties();
+            foreach (var propertyInfo in props)
+            {
+                TryAndForget(null, () =>
+                {
+                    TestContext.Progress.WriteLine($"{pre}{propertyInfo.Name}: {propertyInfo.GetValue(obj)}");
+                });
+            }
+        }
+
+        void TryAndForget(string caption, Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                if (caption != null)
+                    Console.WriteLine($"Fail. {caption}. {ex.GetType().Name}: {ex.Message}");
+            }
 
         }
     }
