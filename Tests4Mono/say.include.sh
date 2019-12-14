5feb84f092f8698673e4f85593dc9e5e5d99cf02
devizer <devizer@users.github.com>
2019-12-14 15:05:01 +0200

    say.include.sh for freebsd

--- Tests4Mono/say.include.sh
+++ Tests4Mono/say.include.sh
@@ -2,9 +2,9 @@ counter=0;
 function header() {
   startAt=${startAt:-$(date +%s)}; elapsed=$(date +%s); elapsed=$((elapsed-startAt));
   if [[ $(uname -s) != Darwin ]]; then
-     elapsed=$(TZ=UTC date -d "@${elapsed}" "+%_H:%M:%S");
+     elapsed=$(TZ=UTC date -d "@${elapsed}" "+%_H:%M:%S" 2>/dev/null);
   else 
-     elapsed=$(TZ=UTC date -r "${elapsed}" "+%_H:%M:%S");
+     elapsed=$(TZ=UTC date -r "${elapsed}" "+%_H:%M:%S" 2>/dev/null);
   fi
   LightGreen='\033[1;32m'; Yellow='\033[1;33m'; RED='\033[0;31m'; NC='\033[0m'; LightGray='\033[1;2m';
   printf "${LightGray}${elapsed:-}${NC} ${LightGreen}$1${NC} ${Yellow}$2${NC}\n"; 
