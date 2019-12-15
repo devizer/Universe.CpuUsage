9f90b44ef34714fa976619a0125431cb51e8c515
devizer <devizer@users.github.com>
2019-12-15 17:22:38 +0200

    benchmark with llvm?2

--- Universe.CpuUsage.Banchmark/run-llvm.sh
+++ Universe.CpuUsage.Banchmark/run-llvm.sh
@@ -1,9 +1,12 @@
-# work=$HOME/build/devizer; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage; git pull; cd Universe.CpuUsage.Banchmark 
+# work=$HOME/build/devizer; mkdir -p $work; cd $work; git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage; git pull; bash run-llvm.sh  
 
-msbuild Universe.CpuUsage.Banchmark.csproj /t:rebuild /p:Configuration=Release
+nuget restore || true; dotnet restore || true
+cd Universe.CpuUsage.Banchmark
+msbuild *.csproj /t:rebuild /p:Configuration=Release /v:q
 cd bin/Release/net47
-mono --llvm Universe.CpuUsage.Banchmark.dll
-
+mono --llvm Universe.CpuUsage.Banchmark.exe
+cd ..
+cd ..
 
 
 
