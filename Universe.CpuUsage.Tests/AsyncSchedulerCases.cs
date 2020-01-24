5ec3630698e898b9de2b385ba7d9174838cdda19
devizer <devizer@users.github.com>
2020-01-24 12:21:27 +0200

    added tests against multiple TaskSchedulers for CpuUsageAsyncWatcher_Tests.cs again

--- Universe.CpuUsage.Tests/AsyncSchedulerCases.cs
+++ Universe.CpuUsage.Tests/AsyncSchedulerCases.cs
@@ -13,11 +13,11 @@ namespace Universe.CpuUsage.Tests
                 var defaultScheduler = TaskScheduler.Default;
                 yield return new AsyncSchedulerCase("Default .NET Scheduler", new TaskFactory(defaultScheduler), defaultScheduler);
 
-                ThreadPerTaskScheduler tpt = new ThreadPerTaskScheduler();
-                yield return new AsyncSchedulerCase("Thread Per Task Scheduler", new TaskFactory(tpt), tpt);
-
                 if (CrossInfo.ThePlatform == CrossInfo.Platform.Windows)
                 {
+                    ThreadPerTaskScheduler tpt = new ThreadPerTaskScheduler();
+                    yield return new AsyncSchedulerCase("Thread Per Task Scheduler", new TaskFactory(tpt), tpt);
+
                     QueuedTaskScheduler s = new QueuedTaskScheduler(1);
                     yield return new AsyncSchedulerCase("Single Threaded QueuedTaskScheduler", new TaskFactory(s), s);
 
