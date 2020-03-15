61a05b6194b7a42345b391a04b09f7f7aebfec00
devizer <devizer@users.github.com>
2020-03-15 03:44:11 +0200

    .drone.yml final?

--- build-and-test-in-docker.sh
+++ build-and-test-in-docker.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
-apt-get update -q; apt-get install -y wget p7zip-full sudo procps bsdutils;
+apt-get update -q; apt-get install -y wget p7zip-full sudo procps bsdutils util-linux;
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash;
 Say "ENVIRONMENT"; printenv | sort
 Say "MEMORY INFO"; free -m; 
 Say "BLOCK STORAGE DEVICES"; sudo fdisk -l || true; 
 Say "MOUNTS"; df -h -T
+Say "LS CPU"; lscpu || true
 Say "7Z BENCHMARK"; 
 if [[ $(uname -m) == armv7* ]]; then
     7z -mmt1 b || true
