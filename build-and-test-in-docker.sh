4d179b3a3751e16dbafeaa0020855ef686b7a37d
devizer <devizer@users.github.com>
2020-03-15 03:31:04 +0200

    .drone.yml final?

--- build-and-test-in-docker.sh
+++ build-and-test-in-docker.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -e
-apt-get update -q; 
-apt-get install -yq locales systemd apt-utils apt-transport-https ca-certificates curl libcurl3 gnupg2 software-properties-common htop mc lsof unzip net-tools bsdutils sudo p7zip-full wget git time ncdu tree procps p7zip-full jq pv; 
-apt-get clean;
-
+apt-get update -q; apt-get install -y wget p7zip-full sudo;
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash;
+Say "ENVIRONMENT"; printenv | sort
 Say "MEMORY INFO"; free -m; 
 Say "BLOCK STORAGE DEVICES"; sudo fdisk -l; 
 Say "MOUNTS"; df -h -T
@@ -14,7 +12,15 @@ if [[ $(uname -m) == armv7* ]]; then
 else
     7z b || true
 fi
-script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash;
-sudo ln -f -s /usr/share/dotnet/dotnet /usr/local/bin/dotnet
+
+apt-get install -yq locales systemd apt-utils apt-transport-https ca-certificates curl libcurl3 gnupg2 software-properties-common htop mc lsof unzip net-tools bsdutils sudo p7zip-full wget git time ncdu tree procps p7zip-full jq pv; 
+apt-get clean;
+if [[ $(uname -m) != armv8l ]]; then
+  script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash;
+else
+  Say "net core is not yet"
+fi
+sudo ln -f -s /usr/share/dotnet/dotnet /usr/local/bin/dotnet || true
+
 Say "TESTING Universe.CpuUsage"
 dotnet test -f netcoreapp2.2
