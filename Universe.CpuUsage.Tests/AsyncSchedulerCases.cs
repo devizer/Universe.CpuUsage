adbfc2523dcb8e6efa70a661d513597b3dc1d2b2
devizer <devizer@users.github.io>
2020-05-08 22:25:46 +0300

    clarifying tests

--- Universe.CpuUsage.Tests/AsyncSchedulerCases.cs
+++ Universe.CpuUsage.Tests/AsyncSchedulerCases.cs
@@ -13,13 +13,13 @@ namespace Universe.CpuUsage.Tests
                 var defaultScheduler = TaskScheduler.Default;
                 yield return new AsyncSchedulerCase("Default .NET Scheduler", new TaskFactory(defaultScheduler), defaultScheduler);
 
-                // works on OSX 10.10 on mono and windows, doesnt work on dotnet except of ConcurrentTest
-                // Fail CpuUsageAsyncWatcher_Tests.ParallelTests and CpuUsageAsyncWatcher_Tests.SimpleTest on Linux
+                // works on OSX 10.10 on mono and windows, may not work on dotnet except of ConcurrentTest
+                // May fail CpuUsageAsyncWatcher_Tests.ParallelTests and CpuUsageAsyncWatcher_Tests.SimpleTest on Linux
                 ThreadPerTaskScheduler tpt = new ThreadPerTaskScheduler();
                 yield return new AsyncSchedulerCase("Thread Per Task Scheduler", new TaskFactory(tpt), tpt);
                 
                 LimitedConcurrencyLevelTaskScheduler lc = new LimitedConcurrencyLevelTaskScheduler(16);
-                yield return new AsyncSchedulerCase("Limited Concurrency Scheduler", new TaskFactory(lc), lc);
+                yield return new AsyncSchedulerCase("Limited Concurrency Scheduler, up to 16 threads", new TaskFactory(lc), lc);
 
                 LimitedConcurrencyLevelTaskScheduler lc1 = new LimitedConcurrencyLevelTaskScheduler(1);
                 yield return new AsyncSchedulerCase("Limited Concurrency Scheduler. Single Thread", new TaskFactory(lc1), lc1);
@@ -29,7 +29,7 @@ namespace Universe.CpuUsage.Tests
                     QueuedTaskScheduler s = new QueuedTaskScheduler(1);
                     yield return new AsyncSchedulerCase("QueuedTaskScheduler, Single Thread", new TaskFactory(s), s);
 
-                    // Fails CpuUsageAsyncWatcher_Tests.ParallelTests only
+                    // May fail CpuUsageAsyncWatcher_Tests.ParallelTests only
                     QueuedTaskScheduler s2 = new QueuedTaskScheduler();
                     yield return new AsyncSchedulerCase("QueuedTaskScheduler, Unlimited Threads", new TaskFactory(s2), s2);
                 }
