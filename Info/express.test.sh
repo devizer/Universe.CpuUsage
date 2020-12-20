99065b7d2e7488e0b8c631bcca7328a399000173
vhoida <vhoida@inscyth.com>
2020-12-20 13:38:20 +0200

    Notifying single threaded restore

---
+++ Info/express.test.sh
@@ -0,0 +1,7 @@
+﻿docker run -it --rm ubuntu bash -c 'export DEBIAN_FRONTEND=noninteractive; apt-get update; apt-get install git curl sudo mc -y;
+  git clone https://github.com/devizer/Universe.CpuUsage; cd Universe.CpuUsage;
+  cd Universe.CpuUsage/Universe.CpuUsage.Tests;
+  script=https://raw.githubusercontent.com/devizer/test-and-build/master/lab/install-DOTNET.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash;
+  dotnet test -f netcoreapp3.1;
+  bash'
+
