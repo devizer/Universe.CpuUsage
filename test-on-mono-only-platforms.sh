8447bdbaf0159c6bb8f4e18a9c0e92c8c7fe4cb4
devizer <devizer@github.users>
2021-11-07 16:28:17 +0200

    just trigger build v8

--- test-on-mono-only-platforms.sh
+++ test-on-mono-only-platforms.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e; #v7
+set -e; #v8
 
 if [[ "$(command -v Reset-Target-Framework)" == "" ]]; then
   script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; 
